c412ef74dd3b3154b11f96edb62d2ef6412578b1
nahid <nahid@tickleright.in>
2024-12-17 12:05:09 +0530

    add search

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
 import Loader from './common/Loader';
 import routes from './routes';
+import { SearchProvider } from './components/SearchContext';
 
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
 
@@ -26,27 +27,30 @@ function App() {
         reverseOrder={false}
         containerClassName="overflow-auto"
       />
-      <Routes>
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/auth/signup" element={<SignUp />} />
-        <Route element={<DefaultLayout />}>
-          <Route index element={<ECommerce />} />
-          {routes.map((routes, index) => {
-            const { path, component: Component } = routes;
-            return (
-              <Route
-                key={index}
-                path={path}
-                element={
-                  <Suspense fallback={<Loader />}>
-                    <Component />
-                  </Suspense>
-                }
-              />
-            );
-          })}
-        </Route>
-      </Routes>
+      <SearchProvider>
+        <Routes>
+          <Route path="/auth/signin" element={<SignIn />} />
+          <Route path="/auth/signup" element={<SignUp />} />
+          <Route element={<DefaultLayout />}>
+            <Route index element={<ECommerce />} />
+            {routes.map((routes, index) => {
+              const { path, component: Component } = routes;
+              return (
+                <Route
+                  key={index}
+                  path={path}
+                  element={
+                    <Suspense fallback={<Loader />}>
+                      <Component />
+                    </Suspense>
+                  }
+                />
+              );
+            })}
+
+          </Route>
+        </Routes>
+      </SearchProvider >
     </>
   );
 }
