deef6a702e918421102e7a6d669e71c446d1faa8
vicky <vicky@tickleright.in>
2025-05-05 15:06:31 +0530

    change coloor

--- src/components/ClassRooms.tsx
+++ src/components/ClassRooms.tsx
@@ -23,49 +23,51 @@ function ClassRooms() {
     return (
         <>
 
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:bg-black text-black dark:text-white dark:bg-gray-800">
-                        All Actives Venues
-                    </caption>
-                    <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                PI ID
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Venue Name
-                            </th>
-                            <th scope="col" className="px-6 py-3 text-right">
-                            Action
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {ClassRoom && ClassRoom.filter((item) =>
-                            Object.values(item).some((value) =>
-                                String(value)
-                                    .toLowerCase()
-                                    .includes(inputValue.toLowerCase()),
-                            ),
-                        ).map((item,i) => (
-                            <tr key={i} className="bg-white dark:bg-black text-black dark:text-gray border-b dark:bg-gray-800 dark:border-warmGray-500 border-gray">
-                              
-                                <td className="px-6 py-4 font-normal">
-                                {item.pi_id != null && tratom}
-                                {item.pi_id}
-                                </td>
-                                <td className="px-6 py-4">
-                                    {item.classroom_venue_name}
-                                </td>
-                                
-                                <td className="px-6 py-4 text-right">
-                                    <Link to={`../Pis/Batches-Venue/${item.venue_id}`} state={{venue: item['classroom_venue_name']}} className="font-medium bg-trueGray-400 rounded-lg px-2 py-2 text-white dark:text-gray hover:underline">Action</Link>
-                                </td>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg ring-1 ring-gray-900/5 bg-white dark:bg-slate-900">
+                <div className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:bg-slate-900 dark:text-white">
+                    All Actives Venues
+                </div>
+                <div className="px-4">
+                    <table className="p-4 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className="text-gray-700 dark:text-white uppercase dark:text-gray-400 bg-slate-100 dark:bg-slate-800">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    PI ID
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Venue Name
+                                </th>
+                                <th scope="col" className="px-6 py-3 text-right">
+                                    Action
+                                </th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {ClassRoom && ClassRoom.filter((item) =>
+                                Object.values(item).some((value) =>
+                                    String(value)
+                                        .toLowerCase()
+                                        .includes(inputValue.toLowerCase()),
+                                ),
+                            ).map((item, i) => (
+                                <tr key={i} className="p-2 text-black dark:text-gray border-b border-slate-200 dark:border-blueGray-700">
+
+                                    <td className="px-6 py-4 font-normal">
+                                        {item['pi_id'] != null && tratom}
+                                        {item['pi_id']}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {item['classroom_venue_name']}
+                                    </td>
+
+                                    <td className="px-6 py-4 text-right ">
+                                        <Link to={`../Pis/Batches-Venue/${item['venue_id']}`} state={{ venue: item['classroom_venue_name'] }} className="font-medium bg-slate-500  dark:bg-slate-800 rounded-lg px-2 py-2 text-white dark:text-gray hover:underline">Action</Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </>
